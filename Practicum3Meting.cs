7e3ce510da7d84c943053359caed474e73132ed3
annem <33720687+annemik@users.noreply.github.com>
2018-01-19 17:14:56 +0100

    Create Practicum3Meting.cs

---
+++ Practicum3Meting.cs
@@ -0,0 +1,62 @@
+using Android.Widget;             // vanwege Buttons 
+using Android.OS;                 // vanwege Bundle
+using Android.App;                // vanwege Activity
+using Android.Views;              // vanwege View
+using Android.Graphics;           // vanwege OnDraw
+using Android.Content;            // vanwege Context
+using System;                     // vanwege EventHandlers 
+using Android.Hardware;           // vanwege SensorManager
+using Android.Locations;          // vanwege ILocationListener
+using Android.Runtime;            // vanwege GeneratedEnum
+using System.Collections.Generic; // vanwege Lists
+
+namespace App3
+{
+    public class Meting
+    {
+        public DateTime dt;
+        public PointF punt;
+
+        public Meting(DateTime dt, PointF punt) 
+        {
+            this.dt = dt;
+            this.punt = punt;
+            //hier gebruiken we nog ergens ToString voor het bericht (deelknop)
+        }
+
+        public override string ToString()
+        {
+            string res;
+
+            DateTime huidigTijd = this.dt;
+            float huidigTextX = this.punt.X;
+            float huidigTextY = this.punt.Y;
+
+            res = $"🏃 {huidigTijd.ToString("H:mm:ss")}-{huidigTextX} ; {huidigTextY}\n";
+            return res;
+        }
+
+        public static float Snelheid(Meting pt, Meting vorige)
+        {
+            float snelheid = 0;
+            //Afstand berekenen in km
+            float dx = pt.punt.X - vorige.punt.X;
+            float dy = pt.punt.Y - vorige.punt.Y;
+            float afstand = (float)Math.Sqrt(dx * dx + dy * dy) / 1000;
+
+            //Tijd in uren
+            //verschil = new TimeSpan();
+            TimeSpan verschil = pt.dt - vorige.dt;
+            //Console.WriteLine(verschil);
+            //float.Parse(verschil);
+
+            //Snelheid in km/uur
+            snelheid = afstand / (float)verschil.TotalHours;
+            Console.WriteLine(snelheid);
+            //Console.WriteLine((float)verschil.TotalHours);
+            
+            return snelheid;
+        }
+
+    }
+}
